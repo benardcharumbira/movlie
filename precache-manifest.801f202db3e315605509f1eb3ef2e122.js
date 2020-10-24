85cbbb274e0ba4e22f70a9e200c0e4df57106f5e
Benard <benardcharumbira@gmail.com>
2020-10-24 07:29:22 +0200

    Updates

---
+++ precache-manifest.801f202db3e315605509f1eb3ef2e122.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17be7e5b2521cb94ce41539533c41d60",
+    "url": "/movlie/index.html"
+  },
+  {
+    "revision": "9461e899fb729c793263",
+    "url": "/movlie/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "c641e565e01e5380da73",
+    "url": "/movlie/static/css/main.37951477.chunk.css"
+  },
+  {
+    "revision": "9461e899fb729c793263",
+    "url": "/movlie/static/js/2.58d40b2c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movlie/static/js/2.58d40b2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c641e565e01e5380da73",
+    "url": "/movlie/static/js/main.c32d309e.chunk.js"
+  },
+  {
+    "revision": "5e42501880a48d161b88",
+    "url": "/movlie/static/js/runtime-main.82273fcf.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/movlie/precache-manifest.801f202db3e315605509f1eb3ef2e122.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/movlie/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
